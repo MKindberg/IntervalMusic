601a982d3a4acb69ebf48cfe8ce6124dde166290
Maurax <marcus3111@gmail.com>
2017-08-12 17:48:21 +0200

    Added button functionality

--- app/src/main/java/mkindberg/intervalmusic/MainActivity.java
+++ app/src/main/java/mkindberg/intervalmusic/MainActivity.java
@@ -2,6 +2,10 @@ package mkindberg.intervalmusic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void editWork(View v){
+        EditText field = (EditText) findViewById(R.id.et_work);
+        if(field.getText().length()==0)
+            field.setText(0);
+        if(((Button) v).getText().equals("+"))
+            field.setText(Integer.toString(Integer.parseInt(field.getText().toString())+1));
+        else
+            field.setText(Integer.toString(Integer.parseInt(field.getText().toString())-1));
+    }
+
+    public void editRest(View v){
+        Log.i("Clicked", "Clicked");
+        EditText field = (EditText) findViewById(R.id.et_rest);
+        if(field.getText().length()==0)
+            field.setText(0);
+        if(((Button) v).getText().equals("+"))
+            field.setText(Integer.toString(Integer.parseInt(field.getText().toString())+1));
+        else
+            field.setText(Integer.toString(Integer.parseInt(field.getText().toString())-1));
+    }
+
+    public void editRounds(View v){
+        EditText field = (EditText) findViewById(R.id.et_rounds);
+        if(field.getText().length()==0)
+            field.setText(0);
+        if(((Button) v).getText().equals("+"))
+            field.setText(Integer.toString(Integer.parseInt(field.getText().toString())+1));
+        else
+            field.setText(Integer.toString(Integer.parseInt(field.getText().toString())-1));
     }
 }
